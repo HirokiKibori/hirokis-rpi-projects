d5efe8636eeda2813358b1ef8808b84ef3bff47b
Uwe Schmidt <uwe-schmidt-dresden@gmx.de>
2015-07-28 09:16:33 +0200

    some structure

--- rpi_state_site/site/index.py
+++ rpi_state_site/site/index.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return 'Server is running ... ^^'
+@app.route('/')
+def index(name=None):
+    return render_template('main_template.html')
+
 
 if __name__ == "__main__": 
     app.run(use_debugger=True, use_reloader=True, debug=True, host='0.0.0.0')
